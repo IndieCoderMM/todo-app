0bad047d7a77e861fc2e02c360b328b4641fa3e5
hthant <hthant00chk@gmail.com>
2022-12-06 15:08:11 +0630

    Load dynamic todolist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import './index.css';
 
-console.log('Hello Webpack!');
+const todoContainer = document.querySelector('#todo-container');
+
+const todoList = [
+  {
+    description: 'Request a code review',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Set up project',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Fix linter errors',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Build an awesome app',
+    completed: false,
+    index: 1,
+  },
+];
+
+const getListItem = (todo) => {
+  const li = document.createElement('li');
+  const checkBox = document.createElement('input');
+  const input = document.createElement('input');
+  checkBox.setAttribute('type', 'checkbox');
+  checkBox.checked = todo.completed;
+  input.value = todo.description;
+  li.appendChild(checkBox);
+  li.appendChild(input);
+  return li;
+};
+
+todoList.sort((a, b) => a.index - b.index);
+todoList.forEach((todo) => {
+  const item = getListItem(todo);
+  todoContainer.appendChild(item);
+});
